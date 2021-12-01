eb9a36cb16ccbe2dddc3cd01c53a72a48c3a05af
VikneshJadhav <viknesh7802@gmail.com>
2021-12-01 10:02:40 +0100

    enfin on a reussi

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,4 +1,4 @@
-
+package principal;
 import java.io.File;
 /**
  * 
@@ -30,7 +30,6 @@ public class Main {
 				System.out.println("  -h, --help         permet d'obtenir de l'aide \n");
 				System.out.println("  -d                 affiche la liste et analyse tous "
 						                                 + "les images a partir d'un certain dossier \n");
-<<<<<<< HEAD
 				System.out.println("  -f                  affiche les metadonnees d'un fichier donnee en parametre \n");
 				System.out.println("  -s                 permet de dissimuler un texte dans un certain fichier \n");
 				System.out.println("  -e                 permet  d'extraire un texte dissimule dans un certain fichier \n");	
@@ -43,12 +42,11 @@ public class Main {
 		case 2:
 			try {
 		    if(args[0].equals("-d") && args[1].equals(".")) {
-				String lechemin = "/mnt/c/Users/vikne/eclipse-workspace/LEPROJETDEJAVAJBSRI/images";
+				String lechemin = "/mnt/c/Users/vikne/eclipse-workspace/PROJETJAVA/images";
 				System.out.println("Liste des images sous : " + lechemin);
 			    System.out.println("------------------------------------");
 				ExploRep explorer = new ExploRep(lechemin);
 				explorer.maListe();
-				System.out.println(explorer.toString());
 			}else {
 				File rep = new File(args[1]);
 				String lechemin = rep.getAbsolutePath(); // sa marche mais il faut mettre le repertoire en question dans src
@@ -56,8 +54,6 @@ public class Main {
 			    System.out.println("------------------------------------");
 			    ExploRep explorer = new ExploRep(lechemin);
 				explorer.maListe();
-				System.out.println(explorer.toString());
-
 			} // ajouter une erreur (a voir)
 			}catch(IllegalArgumentException error1) {
 				System.err.println(error1.getMessage());
@@ -71,29 +67,3 @@ public class Main {
 
 }
 }
-=======
-				System.out.println("  -s                 permet de dissimuler un texte dans un certain fichier \n");
-				System.out.println("  -e                 permet  d'extraire un texte dissimule dans un certain fichier \n");	
-			}
-			break;
-		case 2:
-		    if(args[0].equals("-d") && args[1].equals(".")) {
-				String lechemin = "/mnt/c/Users/srigu/OneDrive/Bureau/ProjetS3/PROJET_de_Jean_Sri";
-				System.out.println("Liste des images sous : " + lechemin);
-			    System.out.println("------------------------------------");
-				Explorepertoire explorer = new Explorepertoire(lechemin);
-				explorer.maListe();
-			}else {
-				File rep = new File(args[1]);
-				String lechemin = rep.getAbsolutePath(); // sa marche mais il faut mettre le repertoire en question dans src
-				System.out.println("Liste des images sous : " + lechemin);
-			    System.out.println("------------------------------------");
-				Explorepertoire explorer = new Explorepertoire(lechemin);
-				explorer.maListe();
-			} // ajouter une erreur (a voir)
-		    break;	
-		}
-	}
-
-}
->>>>>>> 6d89297cd15a64e26359055b1515374d4c00c5e7
